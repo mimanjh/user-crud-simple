e807a13869d0e7e288bf3c7d936a3a6190f2200d
Jacob Hunsaker <jacob.hunsaker96@gmail.com>
2022-04-28 20:33:23 +0900

    Add basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (name, age) => {
+    setUserList((prevUsersList) => {
+      return [...prevUsersList, {name: name, age: age, id: Math.random().toString() }];
+    });
+  }
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
